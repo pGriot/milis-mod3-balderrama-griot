4f25f1d1b5df5050b3e8bd47e9a44614b4b5ee3a
Pablo Griot <pgriot@gmail.com>
2022-10-15 02:34:44 -0300

    reemplazo de BrowserRouter a HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { ClimaApp } from './ClimaApp';
 import './styles.css';
@@ -8,9 +8,9 @@ import './styles.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
         <ClimaApp />
-    </BrowserRouter>    
+    </HashRouter>    
   </React.StrictMode>
 );
 
